242f69b845833d6992e9c337fe7ebf9eddd1fa0e
koji.kumanami <kuma.koji922@gmail.com>
2015-10-06 17:44:37 +0900

    Assetテーブル参照

--- app/Controller/AssetController.php
+++ app/Controller/AssetController.php
@@ -8,6 +8,36 @@ class AssetController extends AppController {
 
     public function index() {
         $this->set('page_title','保有資産');
+        $Asset = ClassRegistry::init('Asset');
+
+        $joins = array(
+           array(
+                'type' => 'inner',
+                'table' => 'issuelist',
+                'alias' => 'Issuelist',
+                'conditions' => array(
+                        'Issuelist.policynum = Asset.policynum'
+                ),
+            )
+        );
+        $assets = $Asset->find('all',
+            array(
+                'joins' => $joins,
+                'alias' => 'Asset',
+                'fields' => Array('Issuelist.name','Issuelist.businesstype','Issuelist.compareyd','Issuelist.stockprice','Asset.num')
+        ));
+        $total = 0;
+        $company_num = 0;
+
+        foreach ( $assets as $asset ){
+            $total += $asset['Asset']['num'] * $asset['Issuelist']['stockprice'];
+            $company_num += 1;
+        }
+
+        $this->set('assets',$assets);
+        $this->set('total',$total);
+        $this->set('company_num',$company_num);
+
 
     }
 }
